5f4d700af01412c669e5a938c6c28ef2a667d2f7
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-09-16 23:11:52 +0100

    Use reqparse to update Course Resource

--- resources/courses.py
+++ resources/courses.py
@@ -34,6 +34,22 @@ class CourseList(Resource):
 class Course(Resource):
     def get(self, id):
       return jsonify({'title': 'Python Basics'})
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'title',
+            required=True,
+            help='No course title provided',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'url',
+            required=True,
+            help='No course URL provided',
+            location=['form', 'json'],
+            type=inputs.url
+        )
+        super().__init__()
 
     def put(self, id):
       return jsonify({'title': 'Python Basics'})
